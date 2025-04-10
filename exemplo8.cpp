8e0067c624e8ab00c0e650d0cccb6a37994a5758
Spet001 <86019227+Spet001@users.noreply.github.com>
2025-04-10 20:30:03 -0300

    Add files via upload

---
+++ exemplo8.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+//verificar par ou impar (resto da divisao)
+main()
+{
+	int n;
+	printf("Entre com um numero:");
+	scanf("%i",&n);
+	if(n%2==0) //% resto da divisao 
+		printf("Par");
+	else
+		printf("Impar");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
