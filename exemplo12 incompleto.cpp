ea43a5ffc0a71cfd2149d871d0e72ced88ab3784
Spet001 <86019227+Spet001@users.noreply.github.com>
2025-04-10 21:59:04 -0300

    Add files via upload

---
+++ exemplo12 incompleto.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main()
+{
+	int cont, idade;
+	char nome[20];
+	char continua='S';
+	printf("Cadastrando clientes da loja \n");
+	while(continua=='S') //Só continua em "Sim"
+	{
+		printf("Digite o nome do cliente:");
+		scanf("%s",&nome);
+		printf("Digite a idade do cliente:");
+		scanf("%i",&idade);
+		cont=cont+1; //contador
+		printf("Deseja continuar? S/N:");
+		scanf("%s",&continua);
+	}
+	printf
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
