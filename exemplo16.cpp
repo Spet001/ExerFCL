172a773a7e17cf0a1f1929c7860997c677e9a071
Spet001 <86019227+Spet001@users.noreply.github.com>
2025-04-24 21:23:53 -0300

    Add files via upload

---
+++ exemplo16.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main()
+{
+	int cont,n; 
+
+	for(cont=1;cont<=10;cont++){ //contador começando em 1 que vai até 10
+	
+		printf("==TABUADA DO %i ==\n",cont);
+		for(n=1;n<=10;n++) //contador começando em 1 que vai até 10
+		
+		printf("%i X %i=%i  \n \n",cont,n,cont*n); //mascara sempre acompannha tudo
+	
+
+}	
+	
+	
+	
+}
